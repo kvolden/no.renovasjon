a08270e3e9e5b94c8c36a13e089789da2b37e4ad
Kjetil Volden <kvolden@gmail.com>
2025-09-03 07:05:29 +0200

    Fix bug where 'fetch' is missing on Homey 2019

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const Homey = require('homey');
+// Node.js v12 (Homey 2019) does not have fetch built-in
+if (typeof fetch === 'undefined') {
+  global.fetch = require('node-fetch');
+}
 
 module.exports = class RenovasjonApp extends Homey.App {
 
